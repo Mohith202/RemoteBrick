da827f1e106e6a23582e1af360b7f7d8063ea9bb
K Mohith Goud <128036418+Mohith202@users.noreply.github.com>
2024-06-15 11:58:30 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,9 +1,9 @@
 import bcrypt
 
-# Helper function to hash passwords
+# to hash password
 def hash_password(password: str) -> str:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-# Helper function to verify passwords
+# to verify passwords
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
